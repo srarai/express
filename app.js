5184c7ddef652bba653d3c1eb843a0ff753cf12f
srarai <yclass6@gmail.com>
2023-02-03 14:59:22 +0530

    bodyparser

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const http= require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-});
 
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/',(req,res,next)=>{
+    next();
+});
+app.use('/add-product',(req,res,next)=>{
     console.log('In another middleware');
-    res.send('<h1>Hello from Express </h1>')
+    res.send('<form action="/product" method="POST"><label for="title">Add Product - </label><input type="text" name="title"><br><label for="quantity">Quantity -  </label><input type="text" name="quantity"><br><button type="sbumit">Submit</button></form>');
 });
 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/',(req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express </h1>');
+});
 app.listen(3000);
